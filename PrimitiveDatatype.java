3e949772fd90a52cb467c0b20261d55947541d8a
Pujarinee Biswal <158996120+pujarinee@users.noreply.github.com>
2025-06-17 13:23:39 +0530

    Add files via upload

---
+++ PrimitiveDatatype.java
@@ -0,0 +1,22 @@
+class PrimitiveDatatype{
+public static void main(String[]args){
+
+	boolean isvalid=true;
+	byte b=50;
+	short c=215;
+	int d=3456789;
+	long l=234567;
+	float pi=3.14f;
+	double g=9078.9;
+	char h = 'A';
+	System.out.println("Boolean value is" +isvalid);
+	System.out.println("Byte value is" +b);
+	System.out.println("Short value is" +c);
+	System.out.println("Integer value is" +d);
+	System.out.println("Long value is" +l);
+	System.out.println("Float value is" +pi);
+	System.out.println("Double value is" +g);
+	System.out.println("Character value is" +h);
+}
+}
+
